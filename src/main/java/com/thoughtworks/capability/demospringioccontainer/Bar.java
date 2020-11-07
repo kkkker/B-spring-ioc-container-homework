85f32bfc5d02706455fe8c3f3e4406fef679da12
kker <907765631@qq.com>
2020-11-07 19:29:19 +0800

    [Mamingjun] feat: implement p3s3

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,11 +1,16 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Bar {
+public class Bar implements ApplicationContextAware, InitializingBean {
 
     private Foo foo;
+    private ApplicationContext context;
 
     public Bar(Foo foo) {
         this.foo = foo;
@@ -17,5 +22,15 @@ public class Bar {
 
     public String name() {
         return "Bar";
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        foo = context.getBean(Foo.class);
+    }
+
+    @Override
+    public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
     }
 }
