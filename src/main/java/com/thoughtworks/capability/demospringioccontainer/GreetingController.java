3ba6d84f537c8cbc7293eaa65a964f0790065cf5
kker <907765631@qq.com>
2020-11-07 19:10:23 +0800

    [Mamingjun] feat: implement p2s2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,25 +1,32 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
-public class GreetingController {
+public class GreetingController implements ApplicationContextAware {
 
-    private final GreetingService greetingService;
+    private ApplicationContext applicationContext;
 
-    @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
+    public GreetingService getPrototypeBean() {
+        return applicationContext.getBean(GreetingService.class);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext)
+            throws BeansException {
+        this.applicationContext = applicationContext;
     }
 
     @GetMapping("/greet")
     public String greet() {
-        return greetingService.sayHi();
+        return applicationContext.getBean(GreetingService.class).sayHi();
     }
 
 }
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,8 +1,11 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class GreetingService {
 
     public GreetingService() {
